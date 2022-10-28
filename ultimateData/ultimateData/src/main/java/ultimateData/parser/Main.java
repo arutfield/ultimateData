249eea4a9c0a8bdb0ab91b6771adb1dccf7c06d2
arutfield <36383013+arutfield@users.noreply.github.com>
2022-10-27 21:18:37 -0400

    initial commit

---
+++ ultimateData/ultimateData/src/main/java/ultimateData/parser/Main.java
@@ -0,0 +1,22 @@
+
+package ultimateData.parser;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.debug("Hello from Log4j 2");
+        logger.debug("{}", () -> getNumber());
+
+    }
+
+    static int getNumber() {
+        return 5;
+    }
+
+}
